f5030bed30ad24179ba002a6953b897a7dcd36a2
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2026-02-07 23:03:04 +0530

    Add Codec class with encode and decode methods
    Implement Codec class for encoding and decoding strings.

--- src/python/learning/one_pager_notes.py
+++ src/python/learning/one_pager_notes.py
@@ -206,3 +206,38 @@ class Solution:
 
 
 
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        encode = []
+        for s in strs:
+             encode.append(f"{len(s)}#{s}")
+        
+        return ''.join(encode)
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        ans = []
+        curr = ""
+        i = 0
+        while i < len(s):
+
+            j = i
+            while s[j] != '#':
+               j  += 1
+            
+            sz = int(s[i:j])
+            ans.append(s[j+1:j+1+sz])
+            i = j+1+sz
+
+        return ans
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
+
