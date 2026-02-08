a31433bf4de1a781f02324c9e852686036f27cf1
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2026-02-08 23:49:26 +0530

    Add letterCombinations method to Solution class

--- src/python/learning/one_pager_notes.py
+++ src/python/learning/one_pager_notes.py
@@ -291,5 +291,37 @@ class Solution:
         
                 
 
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+           return []
+
+        # Mapping of digits to letters
+        key_map = {
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
+        }
+        ans = []
+        res = []
+        def solve(res:List[str],idx:int)->None:
+            if idx == len(digits):
+                ans.append(''.join(res))
+                return
+            s = key_map.get(digits[idx],"")
+            for c in s:
+                res.append(c)
+                solve(res,idx+1)
+                res.pop()
+        solve(res,0)
+        return ans
+
+        
+        
         
 
